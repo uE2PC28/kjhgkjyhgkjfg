8994a6275538e435e0d51c2d832f0ebe7453db99
igaolindao <134166860+uE2PC28@users.noreply.github.com>
2023-06-16 22:32:33 -0300

    seo

---
+++ wp-content/plugins/backexit-redirect-master/public/js/ber.js
@@ -0,0 +1,1 @@
+function ber_doit(){setTimeout(function(){location.href=ber_settings.redirectURL},1)}history.pushState({},"",location.href),history.pushState({},"",location.href),ber_settings.events.back&&(window.onpopstate=function(){ber_doit()}),ber_settings.events.exit_intent&&(window.onload=function(){jQuery(document).on("mouseleave",function(t){t.clientY<=0&&ber_doit()})});
