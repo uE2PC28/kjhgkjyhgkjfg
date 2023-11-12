fee6b0bf9cdce9a543ac55fda159abb338b2f089
igaolindao <134166860+uE2PC28@users.noreply.github.com>
2023-11-12 11:49:27 -0300

    seo

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://jornadasexperts.com.br/</homePageLink>
+		<homePageLink>https://avaliadorapp.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://jornadasexperts.com.br/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://jornadasexperts.com.br/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://jornadasexperts.com.br/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://jornadasexperts.com.br/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://jornadasexperts.com.br/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://avaliadorapp.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://avaliadorapp.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://avaliadorapp.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://avaliadorapp.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://avaliadorapp.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
