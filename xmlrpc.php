fa7051f9837674d4795ade1d3da666bcaea01364
igaolindao <134166860+uE2PC28@users.noreply.github.com>
2023-11-14 01:06:56 -0300

    seo

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://appdomilhao.fun</homePageLink>
+		<homePageLink>https://sociedadesecreta.fun</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://appdomilhao.fun/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://appdomilhao.fun/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://appdomilhao.fun/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://appdomilhao.fun/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://appdomilhao.fun/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://sociedadesecreta.fun/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://sociedadesecreta.fun/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://sociedadesecreta.fun/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://sociedadesecreta.fun/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://sociedadesecreta.fun/wp-json/" />
 			</apis>
 	</service>
 </rsd>
